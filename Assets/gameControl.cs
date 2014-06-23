02af5021cc5c91282a9b7f18e186c2cbd71642a9
mwbullen <mwbullen@new-host-2.home>
2014-06-23 11:35:14 -0400

    Demo of "humans" running through streets
    1.  There are 4 invisible objects called "centers".  Each human
    randomly picks a center as a target and navmesh moves them towards
    that.  When they reach the target they randomly pick a new target and
    thus are in constant motion.
    2.  # of humans is configurable on the MainCamera script value "Human
    Count"

---
+++ Assets/gameControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameControl : MonoBehaviour {
+	public GameObject Human;
+	public int humanCount;
+
+
+	// Use this for initialization
+	void Start () {
+		loadHumans ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	//Create humans per HumanCount
+	void loadHumans() {
+	
+		for (int i = 0; i<humanCount; i++) {
+
+			float x = Random.Range(30, 70);
+			float z = Random.Range(30, 70);
+
+			Quaternion q = Quaternion.Euler (0,0,0);
+
+			Instantiate(Human, new Vector3(x, .5f, z), q);
+		}
+	}
+
+}
---
+++ Assets/humanAi.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class humanAi : MonoBehaviour {
+	Vector3 direction;
+
+	public NavMeshAgent navAgent;
+
+	GameObject currentTarget;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//set Random rotation for visual diversity
+		transform.rotation = getRandomRotation ();
+
+		//load NavAgent
+		navAgent = (NavMeshAgent)GetComponent("NavMeshAgent");
+
+		//Pick a random target from the designated objects
+		currentTarget = getRandomNavTarget ();
+
+		//Go to new target
+		navAgent.SetDestination(currentTarget.transform.position);
+	}
+
+
+	Quaternion getRandomRotation() {
+		int y = Random.Range (0, 360);
+		
+		Quaternion q = Quaternion.Euler (0, y, 0);
+		
+		return q;
+		
+	}
+
+	//return randomly selected nav target
+	GameObject getRandomNavTarget() {
+		GameObject[] targets = GameObject.FindGameObjectsWithTag ("Finish");
+
+		int x = Random.Range (0, targets.Length);
+
+		return targets [x];
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//If close to nav destination, pick new random destination to keep moving
+		if (navAgent.remainingDistance < 5) {
+						currentTarget = getRandomNavTarget ();
+						navAgent.SetDestination (currentTarget.transform.position);
+				}
+
+		}
+
+
+}
